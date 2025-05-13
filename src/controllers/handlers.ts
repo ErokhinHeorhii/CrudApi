386136df03d66c59854e5b74ac58627a0f416a85
Heorhi Yarokhin <heorhi_yarokhin@senla.eu>
2025-05-13 23:02:23 +0300

    feat: add  setReqRes for test scenario

--- src/controllers/handlers.ts
+++ src/controllers/handlers.ts
@@ -4,21 +4,20 @@ import { userRoutes } from '../routes/routes.ts';
 import { setReqRes } from './controller.ts';
 
 export const handleUserRequest = async (req: IncomingMessage, res: ServerResponse) => {
-    const url = parse(req.url || '', true);
-    const pathname = url.pathname || '';
-    const method = req.method || '';
+  const url = parse(req.url || '', true);
+  const pathname = url.pathname || '';
+  const method = req.method || '';
 
-    const idMatch = pathname.match(/^\/api\/users\/([a-zA-Z0-9-]+)$/);
-    const routeKey = idMatch ? `${method}:/api/users/:id` : `${method}:${pathname}`;
+  const idMatch = pathname.match(/^\/api\/users\/([a-zA-Z0-9-]+)$/);
+  const routeKey = idMatch ? `${method}:/api/users/:id` : `${method}:${pathname}`;
 
-    const handler = userRoutes[routeKey];
+  const handler = userRoutes[routeKey];
 
+  if (handler) {
     setReqRes(req, res);
-
-    if (handler) {
-        await handler();
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Route not found' }));
-    }
+    await handler();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
+  }
 };
